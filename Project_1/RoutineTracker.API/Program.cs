f913489f11bb3126cfb23b23d08dfb07ce9d38d0
JtheFox <60980739+JtheFox@users.noreply.github.com>
2024-12-26 10:32:18 -0500

    Add injections in Program.cs

--- Project_1/RoutineTracker.API/Program.cs
+++ Project_1/RoutineTracker.API/Program.cs
@@ -1,11 +1,36 @@
 using Microsoft.EntityFrameworkCore;
+using RoutineTracker.API.Data;
+using RoutineTracker.API.Repository;
+using RoutineTracker.API.Service;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add dbcontext and connect it to connection string
+builder.Services.AddDbContext<RoutineTrackerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RoutineTracker")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Dependency Inject the proper services
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IItemService, ItemService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+//Dependency Inject the proper repositories
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IItemRepository, ItemRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+//Add our controllers
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
+
 
 var app = builder.Build();
 

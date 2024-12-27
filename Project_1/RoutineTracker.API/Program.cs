071732394dfe83437688aa25f46a9c50d239e589
JtheFox <60980739+JtheFox@users.noreply.github.com>
2024-12-27 13:57:54 -0500

    Add controller mapping

--- Project_1/RoutineTracker.API/Program.cs
+++ Project_1/RoutineTracker.API/Program.cs
@@ -53,10 +53,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
